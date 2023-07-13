789ada8a255bd11e5fab5c614bcfb12ac9467cae
Ori Wagmi <ori.wagmi@gmail.com>
2023-07-13 11:46:12 -0700

    add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+      hardhat: {
+      },
+      arbitrum: {
+        url: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ARB_API_KEY}`,
+        accounts: [process.env.DEPLOYER_PRIV_KEY],
+      } 
+    },
+  solidity: {
+    version: "0.8.10",
+    settings: {
+        optimizer: {
+            enabled: false,
+            runs: 200,
+        },
+    },
+  },
 };
